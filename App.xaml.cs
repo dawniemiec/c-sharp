0acdd10e8e05ccd2686e2a11310ac43e0a13df48
dawniemiec <115160733+dawniemiec@users.noreply.github.com>
2024-05-09 19:04:12 +0200

    Add files via upload

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WpfApp1
+namespace lab8_DB
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using lab8_DBEF.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,8 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data;
+using Microsoft.Data.SqlClient;
 
-namespace lab8_DBEF
+namespace lab8_DB
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -22,43 +27,56 @@ namespace lab8_DBEF
             InitializeComponent();
         }
 
-        private void btnRef_Click(object sender, RoutedEventArgs e)
-        {
-            var db = new DbAeroklub();
-            lbxData.ItemsSource = db.Samoloty.ToList();
-            lAvgPulap.Content = db.Samoloty.Average(s=>s.Pulap);
-        }
-
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            var db = new DbAeroklub();
-            string producent = txtProd.Text;
-            string model = txtModel.Text;
-            double predk = Convert.ToDouble(txtPredkosc.Text);
-            double pul = Convert.ToDouble(txtPulap.Text);
-            double udzwig = Convert.ToDouble(txtUdzwig.Text);
+            using (var connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Klaster;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"))
+            {
+                string nazwa, nrSer;
+                byte lRdzeni;
+                nazwa = txtName.Text;
+                nrSer = txtSerial.Text;
+                lRdzeni =Convert.ToByte(txtCores.Text);
+                SqlCommand command = new SqlCommand($"INSERT Komputery (Nazwa,NrSeryjny,LiczbaRdzeni) VALUES('{nazwa}','{nrSer}',{lRdzeni})", connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
 
-            Samolot samolot = new Samolot() { Producent = producent, Model = model, Predkosc = predk, Pulap = pul, Udzwig = udzwig };
-            db.Samoloty.Add(samolot);
-            db.SaveChanges();
-            lbxData.ItemsSource = db.Samoloty.ToList();
+            }
 
         }
 
         private void btnDel_Click(object sender, RoutedEventArgs e)
         {
-            var db = new DbAeroklub();
-            int id = Convert.ToInt32(txtId.Text);
-            var samolot = db.Samoloty.Find(id);
-            if (samolot != null)
+            int Id =Convert.ToInt32(txtId.Text);
+            using (var connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Klaster;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"))
             {
-                db.Remove(samolot);
-                db.SaveChanges();
-                lbxData.ItemsSource = db.Samoloty.ToList();
-
-
+                SqlCommand command = new SqlCommand($"DELETE FROM Komputery WHERE Id={Id}", connection);
+                connection.Open();
+                command.ExecuteNonQuery();
             }
 
+
+        }
+
+        private void btnRef_Click(object sender, RoutedEventArgs e)
+        {
+            using (var connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Klaster;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False"))
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM Komputery", connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    lbxComputers.Items.Add($"{reader["Id"]}. {reader["Nazwa"]} | {reader["NrSeryjny"]} | {reader["LiczbaRdzeni"]}");
+                }
+                reader.Close();
+
+                SqlCommand command2 = new SqlCommand("SELECT SUM(LiczbaRdzeni) FROM Komputery", connection);
+                int sumOfCores =(int)command2.ExecuteScalar();
+                lSumCores.Content = sumOfCores;
+                connection.Close();
+
+            }
         }
     }
 }
